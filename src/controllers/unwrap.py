a47b8b1d8e6f5a7123c52b87e1be54ec7fb66b04
Facundo Flores <flores.facundogabriel@exa.unsa.edu.ar>
2016-02-25 17:43:26 -0300

    Fix controller unwrap

--- src/controllers/unwrap.py
+++ src/controllers/unwrap.py
@@ -12,7 +12,7 @@ from traitsui.api import Handler
 class UnwrapHandler(Handler):
     """ Controlador para el ModelView: Unwrap
     """
-    def object_use_use_unwrapping_changed(self, info):
+    def object_use_unwrapping_changed(self, info):
         if info.object.use_unwrapping:
             print "Using Unwrapping"
         else:
